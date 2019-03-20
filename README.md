a66d1489bf81de733db05a345027cc174f4eea9b
Zachary Kohnen <DusterTheFirst@users.noreply.github.com>
2019-03-20 16:20:05 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# GUIShopPlus
---
+++ src/com/dusterthefirst/guishopminus/Plugin.java
@@ -0,0 +1,16 @@
+package com.dusterthefirst.guishopminus;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Plugin extends JavaPlugin {
+    // Fired when plugin is first enabled
+    @Override
+    public void onEnable() {
+    }
+    
+    // Fired when plugin is disabled
+    @Override
+    public void onDisable() {
+
+    }
+}
