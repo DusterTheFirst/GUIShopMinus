81e8da0dc7ef323d3848e16f3a4f96dcdc7cc635
DusterTheFirst <dusterthefirst@users.noreply.github.com>
2019-04-05 17:20:02 -0400

    Do not release new version if same jarfile

--- scripts/github_release.sh
+++ scripts/github_release.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-# # Get the previous hash for the archive
-# latest_release=$(curl -sS -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/repos/$TRAVIS_REPO_SLUG/releases/latest" \
-#     | jq ".assets[] | .name")
+echo $(curl -sS -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/rate_limit" \
+    | jq -r '.resources.core | "\(.remaining) out of \(.limit) requests left until \(.reset | strftime("%Y-%m-%d %H:%M:%S")) UTC"')
 
-# echo $latest_release
-# exit 0
+# Get the previous hash for the archive
+latest_release=$(curl -sS -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/repos/$TRAVIS_REPO_SLUG/releases/latest" \
+    | jq -r ".assets[] | select(.name == \"checksum.sha256\") | .url")
+
+curl -sS -H "Authorization: token $GITHUB_TOKEN" -H "Accept: application/octet-stream" -L $latest_release | shasum -a 256 -c -
+
+if [ $? = 0 ]; then
+    echo "Jarfile has not changed since last build, aborting"
+    exit 0
+fi
+
+# Generate a version
+version=${TRAVIS_TAG:-$(date +'%Y-%m-%d')-$(git log --format=%h -1)}
+
+# Add the tag
+git tag $version
 
 # Create the body of a release
 body="## Changes in this release
@@ -17,11 +30,11 @@ echo "Create release $version for repo: $TRAVIS_REPO_SLUG, branch: $TRAVIS_BRANC
 
 # Generate JSON payload
 payload=$(jq -n --arg body "$body" --arg name "Snapshot $version" --arg tag_name "$version" --arg target_commitish "$TRAVIS_BRANCH" \
-    '{ body: $body, name: $name, tag_name: $tag_name, target_commitish: $target_commitish, draft: false, prerelease: true }')
+    '{ body: $body, name: $name, tag_name: $tag_name, target_commitish: $target_commitish, draft: false, prerelease: false }')
 
 # Create the release and get the file upload URL
 url=$(curl -sS -H "Authorization: token $GITHUB_TOKEN" --data "$payload" "https://api.github.com/repos/$TRAVIS_REPO_SLUG/releases" \
-    | jq .upload_url | sed -e "s/{?name,label}//g" | sed -e "s/\"//g")
+    | jq -r .upload_url | sed -e "s/{?name,label}//g")
 
 # Upload the archive
 curl -sS -H "Authorization: token $GITHUB_TOKEN" -H "Content-Type: application/java-archive" --data @target/GuiShopMinus.jar "$url?name=GuiShopMinus-$version-SNAPSHOT.jar" > /dev/null
--- scripts/plugin_version.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Generate a version
-version=${TRAVIS_TAG:-$(date +'%Y-%m-%d')-$(git log --format=%h -1)}
-
-# Add the tag
-git tag $version
-
-export VERSION=$version
