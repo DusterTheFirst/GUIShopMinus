47ff1d044b5ffaea4ba7a7bc5f5ab5cd069b2729
Zachary Kohnen <DusterTheFirst@users.noreply.github.com>
2019-04-05 00:38:55 -0400

    [travis skip] optomise shell script

--- scripts/github_release_script.sh
+++ scripts/github_release_script.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# Set deployment bot credentials
-git config --local user.name "Deployment Bot"
-git config --local user.email "deploy@travis-ci.org"
-
-# Get environment variables
-branch=$TRAVIS_BRANCH
-repo_slug=$TRAVIS_REPO_SLUG
-token=$GITHUB_TOKEN
-
 # Generate a version
 version=${TRAVIS_TAG:-$(date +'%Y-%m-%d')-$(git log --format=%h -1)}
 
@@ -21,28 +12,15 @@ $(git log --format="- %aN - %s (%h)" $TRAVIS_COMMIT_RANGE)
 > Thank you for your support"
 
 # Echo status
-echo "Create release $version for repo: $repo_slug, branch: $branch"
+echo "Create release $version for repo: $TRAVIS_REPO_SLUG, branch: $TRAVIS_BRANCH"
 
 # Generate JSON payload
-payload=$(jq -n \
-  --arg body "$body" \
-  --arg name "Snapshot $version" \
-  --arg tag_name "$version" \
-  --arg target_commitish "$branch" \
-  '{
-    body: $body,
-    name: $name,
-    tag_name: $tag_name,
-    target_commitish: $target_commitish,
-    draft: false,
-    prerelease: true
-  }')
+payload=$(jq -n --arg body "$body" --arg name "Snapshot $version" --arg tag_name "$version" --arg target_commitish "$TRAVIS_BRANCH" \
+    '{ body: $body, name: $name, tag_name: $tag_name, target_commitish: $target_commitish, draft: false, prerelease: true }')
 
 # Create the release and get the file upload URL
-url=$(curl -sS -H "Authorization: token $token" --data "$payload" "https://api.github.com/repos/$repo_slug/releases" \
-    | jq .upload_url \
-    | sed -e "s/{?name,label}/?name=GuiShopMinus-$version-SNAPSHOT.jar/g" \
-    | sed -e "s/\"//g")
+url=$(curl -sS -H "Authorization: token $GITHUB_TOKEN" --data "$payload" "https://api.github.com/repos/$TRAVIS_REPO_SLUG/releases" \
+    | jq .upload_url | sed -e "s/{?name,label}/?name=GuiShopMinus-$version-SNAPSHOT.jar/g" | sed -e "s/\"//g")
 
 # Upload the archive
-curl -sS -H "Authorization: token $token" -H "Content-Type: application/java-archive" --data @target/GuiShopMinus.jar $url > /dev/null
+curl -sS -H "Authorization: token $GITHUB_TOKEN" -H "Content-Type: application/java-archive" --data @target/GuiShopMinus.jar $url > /dev/null
